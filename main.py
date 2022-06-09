2bc3c3f7035998c09c92a77d27d1b40ca839f068
SERGIO ARRIAGADA <91762685+Sergiowo@users.noreply.github.com>
2022-06-09 19:56:08 +0000

    Mas cosas xD

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-#print("Ingrese la instancia que desea resolver:")
-#opcion = print("Opcion 1 para 'EjemploProfesora'\n Opcion 2 para 'QAP_sko56_04_n'\n Opcion 3 para 'QAP_sko100_04_n'")
-
-
-
 espacioLocales = []
-matrizConcurrencia = []
+matrizClientes = []
 
-def llenarVectorYMatriz():
-  archivoInstancia = open("EjemploProfesora.txt")
+def llenarVectorYMatriz(nombre):
+  archivoInstancia = open(nombre)
   lineaLeer = archivoInstancia.readline().strip().split(",")
   cantidadLocales = int(lineaLeer[0])
 
@@ -15,15 +10,26 @@ def llenarVectorYMatriz():
   for x in lineaLeer:
     espacioLocales.append(int(x))
 
-  for x in range(0,5):
+  for x in range(0,cantidadLocales):
     lineaLeer = archivoInstancia.readline().strip().split(",")
-    for j in range(0,5):
+    for j in range(0,cantidadLocales):
       lineaLeer[j] = int(lineaLeer[j])
-    matrizConcurrencia.append(lineaLeer)
+    matrizClientes.append(lineaLeer)
   return cantidadLocales
+  
 
+print("Ingrese la instancia que desea resolver:")
+opcion = input("Opcion 1 para 'EjemploProfesora'\nOpcion 2 para 'QAP_sko56_04_n'\nOpcion 3 para 'QAP_sko100_04_n' ")
+while(opcion != "1" and opcion != "2" and opcion != "3"):
+  opcion = input("Ingrese una opcion valida: ")
+if(opcion == "1"):
+  nombre = "EjemploProfesora.txt"
+if(opcion == "2"):
+  nombre = "QAP_sko56_04_n.txt"
+if(opcion == "3"):
+  nombre = "QAP_sko100_04_n.txt"
 
-cantidadPuestos = llenarVectorYMatriz()
+cantidadPuestos = llenarVectorYMatriz(nombre)
 print(cantidadPuestos)
 print(espacioLocales)
-print(matrizConcurrencia)
+print(matrizClientes)
